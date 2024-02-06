dd80e22107274e68bb0537d26c4e77fd29bffea4
tinamorlock <tinamorlock@gmail.com>
2024-02-06 14:48:01 -0600

    started on functions for displaying header tag info

--- script.js
+++ script.js
@@ -7,9 +7,59 @@ const myCode = document.querySelector('#myCode');
 
 // style functions
 
-const headingStyles = () => {
+const headingStyles = (element) => {
     // add code to display the style options in the #styleOptions div
-    styleOptions.innerHTML = 'test'; // this code is working and where I stopped
+    styleOptions.innerHTML = `<h2>Style Options for ${element}</h2>` +
+        '<input type="checkbox" name="centered"> ' +
+        '<label for="centered">center</label>' +
+        ' <input type="checkbox" name="add-border">' +
+        ' <label for="add-border">add border</label>' +
+        ' <input type="checkbox" name="upper-case"> ' +
+        ' <label for="upper-case">uppercase</label>' +
+        ' <input type="checkbox" name="lower-case">' +
+        ' <label for="lower-case">lowercase</label>';
+    displayAboutHeading(element);
+
+    // const getStyles = document.getElementById('styleOptions').value;
+    // console.log(getStyles);
+}
+
+const displayAboutHeading = (element) => {
+    switch (element) {
+        case 'H1':
+            h1Info(); // function complete
+            break;
+        case 'H2':
+            h2Info();
+            break;
+        case 'H3':
+            h3Info();
+            break;
+        case 'H4':
+            h4Info();
+            break;
+        case 'H5':
+            h5Info();
+            break;
+        case 'H6':
+            h6Info();
+            break;
+        default:
+            console.log('displayAboutHeading() ran without receiving a valid heading tag');
+    }
+}
+
+const h1Info = () => {
+    aboutElement.innerHTML = '<h3>About the H1 Element</h3>' +
+        'The H1 element is a top-level heading that is generally placed ' +
+            'at the top of a webpage to denote what that page is about. ' +
+            'It should be the most prominent text on your page. ' +
+            'This helps both the user and the search engines understand what ' +
+            'your page is all about.' +
+            '<br><br><strong>Example code for the H1 tag:<br><br></strong>' +
+            '<code>&lt;H1&gt;This is an H1 block&lt;/H1&gt;</code>' +
+            '<br><br><strong>How it displays:<br><br></strong>' +
+            '<H1 style="color:black">This is an H1 block</H1>';
 }
 
 const generatorMenu = () => {
@@ -22,12 +72,12 @@ const generatorMenu = () => {
         case 'H4':
         case 'H5':
         case 'H6':
-            headingStyles();
+            headingStyles(selectedElement);
             break;
     }
 }
 
-// heading event listeners
+// event listeners
 
 myContent.addEventListener('change', generatorMenu);
 
