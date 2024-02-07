f7d965437675f938cd8529be9a8a8c70ec745353
tinamorlock <tinamorlock@gmail.com>
2024-02-06 18:51:57 -0600

    code is not clean yet, but it's functional

--- script.js
+++ script.js
@@ -4,20 +4,26 @@ const myContent = document.querySelector('#myContent');
 const styleOptions = document.querySelector('#styleOptions');
 const aboutElement = document.querySelector('#aboutElement');
 const myCode = document.querySelector('#myCode');
-const htmlCode = document.querySelector('#genCode')
+const htmlCode = document.querySelector('#genCode');
+const customInput = document.querySelector('#customInput');
+
+// style selectors and arrays
+
+
+// const headerStyleArr = ['centered', 'border', 'upper', 'lower'];
 
 // style functions
 
 const headingStyles = (element) => {
-    // add code to display the style options in the #styleOptions div
+
     styleOptions.innerHTML = `<h2>Style Options for ${element}</h2>` +
-        '<input type="checkbox" name="centered"> ' +
+        '<input type="checkbox" name="centered" id="centered"> ' +
         '<label for="centered">center</label>' +
-        ' <input type="checkbox" name="add-border">' +
+        ' <input type="checkbox" name="add-border" id="border">' +
         ' <label for="add-border">add border</label>' +
-        ' <input type="checkbox" name="upper-case"> ' +
+        ' <input type="checkbox" name="upper-case" id="upper"> ' +
         ' <label for="upper-case">uppercase</label>' +
-        ' <input type="checkbox" name="lower-case">' +
+        ' <input type="checkbox" name="lower-case" id="lower">' +
         ' <label for="lower-case">lowercase</label>';
     displayAboutHeading(element);
 
@@ -90,19 +96,86 @@ const h3Info = () => {
         '<H3>This is an H3 block</H3>';
 }
 
+const h4Info = () => {
+    aboutElement.innerHTML = '<h3>About the H4 Element</h3>' +
+        'The H4 element is a fourth-level heading that is used as an ' +
+        'even deeper subheading that is one level down from the ' +
+        'H3 element. You\'re not likely to see these on every single ' +
+        'webpage. However, the H4 tag can be handy when you have ' +
+        'a lot of information to organize.' +
+        '<br><br><strong>Example code for the H4 tag:<br><br></strong>' +
+        '<code>&lt;H4&gt;This is an H4 block&lt;/H4&gt;</code>' +
+        '<br><br><strong>How it displays:<br><br></strong>' +
+        '<H4>This is an H4 block</H4>';
+}
+
+const h5Info = () => {
+    aboutElement.innerHTML = '<h3>About the H5 Element</h3>' +
+        'The H5 element is a fifth-level heading that is used as an ' +
+        'even deeper subheading that is one level down from the ' +
+        'H4 element. You\'re not likely to see these on every single ' +
+        'webpage. However, the H5 tag can be handy when you have ' +
+        'a lot of information to organize.' +
+        '<br><br><strong>Example code for the H5 tag:<br><br></strong>' +
+        '<code>&lt;H5&gt;This is an H5 block&lt;/H5&gt;</code>' +
+        '<br><br><strong>How it displays:<br><br></strong>' +
+        '<H5>This is an H5 block</H5>';
+}
+
+const h6Info = () => {
+    aboutElement.innerHTML = '<h3>About the H6 Element</h3>' +
+        'The H6 element is a sixth-level heading that is used as an ' +
+        'even deeper subheading that is one level down from the ' +
+        'H5 element. It is the lowest-level heading tag available.' +
+        '<br><br><strong>Example code for the H5 tag:<br><br></strong>' +
+        '<code>&lt;H5&gt;This is an H5 block&lt;/H5&gt;</code>' +
+        '<br><br><strong>How it displays:<br><br></strong>' +
+        '<H5>This is an H5 block</H5>';
+}
+
 const headingInput = () => {
-    // code will grab input and save to variable to convert to code later
+    customInput.innerHTML = '<input type="text" id="inputHeader" name="inputHeader"><br>';
 }
 
-const headingCode = (element, styles) => {
-    // code that converts styles in a way that can be parsed into a style attribute
-    const convertedStyles = styleGen(styles);
-    myCode.innerHTML = `&lt;${element} style="${convertedStyles}"&gt;` +
-    userHeader + '/&gt;';
+const headingCode = (element) => {
+    const userHeader = document.querySelector('#inputHeader').value;
+    const convertedStyles = styleGen(element);
+    myCode.innerHTML = '<h1>Here is your generated code:</h1>' +
+        `<code>&lt;${element} style="${convertedStyles}"&gt;` +
+        userHeader + `&lt;/&gt;${element}</code>` +
+        '<br><br>Just copy and paste the code into your HTML editor!' +
+        '<br><br><h2>This is how it will look in the browser:</h2>' +
+        `<${element} style="${convertedStyles}">${userHeader}</${element}>`;
 }
 
-const styleGen = (styles) => {
-    // code will convert styles data into a string that can be inserted into a style attribute
+const styleGen = (element) => {
+    let styleString = '';
+    switch (element) {
+        case 'H1':
+        case 'H2':
+        case 'H3':
+        case 'H4':
+        case 'H5':
+        case 'H6':
+            const centeredCheckbox = document.getElementById('centered');
+            const borderCheckbox = document.getElementById('border');
+            const upperCheckbox = document.getElementById('upper');
+            const lowerCheckbox = document.getElementById('lower');
+            if (centeredCheckbox.checked) {
+                styleString += 'text-align:center;';
+            }
+            if (borderCheckbox.checked) {
+                styleString += 'border: 1px solid black;';
+            }
+            if (upperCheckbox.checked) {
+                styleString += 'text-transform: uppercase;';
+            }
+            if (lowerCheckbox.checked) {
+                styleString += 'text-transform: lowercase;';
+            }
+            break;
+    }
+    return styleString;
 }
 
 const generatorMenu = () => {
@@ -116,15 +189,14 @@ const generatorMenu = () => {
         case 'H5':
         case 'H6':
             headingStyles(selectedElement);
-            headingInput(); // not created yet
+            headingInput();
             break;
     }
 }
 
 const displayCode = () => {
     const selectedElement = myContent.value;
-    const selectedStyles = styleOptions.value;
-    switch (selectedElement, selectedStyles) {
+    switch (selectedElement) {
         case 'H1':
         case 'H2':
         case 'H3':
